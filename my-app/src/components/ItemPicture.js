8a54a9abf7ee9207c78b1af08af5d0994c79fbd5
talshuker <talshuker11@gmail.com>
2022-12-07 00:04:49 +0200

    delete: first task

--- my-app/src/components/ItemPicture.js
+++
@@ -1,7 +0,0 @@
-import './ItemPicture.css'
-
-function ItemPicture(props) {
-    return <div className="item-picture"> <img className="img" src={props.itemPicture} alt="" /> </div>;
-};
-
-export default ItemPicture;
